4f6517731dffa7161248a77e47dc1c50103c1f28
Swagatadas2000 <swagatadas6121@gmail.com>
2020-12-12 22:51:30 +0530

    webpage

---
+++ hackoff/index.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    console.log("working");
+    Particles.init({
+      selector: '.background',
+      connectParticles: true,
+      color: '#0077ff',
+      responsive: [
+        {
+        breakpoint: 800,
+        options: {
+          color: '#0077ff',
+          maxParticles: 80,
+          connectParticles: false}
+        }
+      ]
+    });
+  };
+  
+  
